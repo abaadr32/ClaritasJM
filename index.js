5f6989724548f81ba2ba52732216acd2d72949d4
abaadr32 <127270202+abaadr32@users.noreply.github.com>
2025-03-24 18:18:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var menuToggle = document.getElementById("menu-toggle");
     var menu = document.getElementById("menu");
     var menuOverlay = document.getElementById("menu-overlay");
+    var dropdownToggle = document.getElementById("dropdown-toggle");
+    var dropdown = document.querySelector(".dropdown");
 
     menuToggle.addEventListener("click", function() {
         menu.classList.toggle("active");
@@ -11,5 +13,18 @@ document.addEventListener("DOMContentLoaded", function() {
     menuOverlay.addEventListener("click", function() {
         menu.classList.remove("active");
         menuOverlay.classList.remove("active");
+        dropdown.classList.remove("active");
+    });
+
+    dropdownToggle.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent link from navigating
+        dropdown.classList.toggle("active");
+    });
+
+    // Close dropdown if clicked outside
+    document.addEventListener("click", function(event) {
+        if (!dropdown.contains(event.target) && !dropdownToggle.contains(event.target)) {
+            dropdown.classList.remove("active");
+        }
     });
 });
